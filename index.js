9b28385efc5bd39340c87746d0ead79b949cd996
Yobany Caltempa <gioclt26@gmail.com>
2022-03-16 13:51:21 -0400

    commit tree c0baf43f01bb1e1902118082d77dc47ff67b09b1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(drivers, query) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === query.toLowerCase();
+    })
+};
+
+function fuzzyMatch(drivers, query) {
+    const lengthofQuery = query.length
+    return drivers.filter(function(driver) {
+        return driver.slice(0, lengthofQuery) === query
+    })
+}
+
+function matchName(drivers, name){
+    return drivers.filter(function(hometown){
+      return hometown.name === name
+    })
+  }
